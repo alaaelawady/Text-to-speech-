0e2fc9707732e64fdd3113734566fbdfccecc841
alaaelawady <60554106+alaaelawady@users.noreply.github.com>
2021-04-01 04:08:00 +0200

    Add files via upload

---
+++ Text to speech/Text to speech.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  1 03:23:28 2021
+
+@author: aLaa
+"""
+
+# Import the Gtts module for text  
+# to speech conversion 
+from gtts import gTTS 
+  
+# import Os module to start the audio file
+import os 
+
+fh = open("text.txt", "r")
+myText = fh.read().replace("\n", " ")
+
+# Language we want to use 
+language = 'en'
+
+output = gTTS(text=myText, lang=language, slow=False)
+
+output.save("output.mp3")
+fh.close()
+
+# Play the converted file 
+os.system("start output.mp3")
